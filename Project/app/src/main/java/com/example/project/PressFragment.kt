eaf6c7c094a6d9c8c9cbca29b83fb94b3c862856
Kleity <perezaguirrejazmin@gmail.com>
2020-11-25 02:30:50 -0600

    App v1.0.8

--- Project/app/src/main/java/com/example/project/PressFragment.kt
+++ Project/app/src/main/java/com/example/project/PressFragment.kt
@@ -1,10 +1,17 @@
 package com.example.project
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sdsmdg.harjot.vectormaster.VectorMasterView
+import com.sdsmdg.harjot.vectormaster.models.PathModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +28,9 @@ class PressFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var db : DocumentReference
+    lateinit var db2 : DocumentReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +44,107 @@ class PressFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_press, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_press, container, false)
+        var pressPieDer: VectorMasterView = view.findViewById(R.id.pressPieDer)
+        //var pressPieIzq: VectorMasterView = view.findViewById(R.id.pressPieIzq)
+
+        db = FirebaseFirestore.getInstance().document("TTInsole/users")
+        db2 = FirebaseFirestore.getInstance().document("TTInsole/micros")
+
+        //Obtener el mc asociado al uid
+        val ns = db.collection("${FirebaseAuth.getInstance().uid}").document("ns")
+        ns.get().addOnSuccessListener { document ->
+            if (document != null) {
+                Log.d("TEST", "DocumentSnapshot data: ${document.data}")
+                val nsd = document.getString("nsd")
+                val nsi = document.getString("nsi")
+                //Obtener el array de temperatura Derecha
+                val pressd = db2.collection("ns").document("${nsd}").collection("${FirebaseAuth.getInstance().uid}").document("press")
+                pressd.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null){
+                            val list_press = document["pder"] as ArrayList<Number>?
+                            Log.d("PRUEBAMESTA", list_press.toString())
+
+                            if (list_press != null) {
+                                //Establecer rango de color segun el valor leido
+                                var a = 0
+                                for (p in list_press) {
+                                    a = a + 1
+                                    if(p.toInt() <= 2){
+                                        //println(p)
+                                        //println("rango 1")
+                                    }
+                                    if(p.toInt() in 3..7){
+                                        var opd1: PathModel = pressPieDer.getPathModelByName("opd" + a)
+                                        opd1.setFillColor(Color.parseColor("#fcba03"))
+                                        //println(p)
+                                        //println("rango 2")
+                                    }
+                                    if(p.toInt() > 7){
+                                        var opd2: PathModel = pressPieDer.getPathModelByName("opd" + a)
+                                        opd2.setFillColor(Color.parseColor("#ED4337"))
+                                        //println(p)
+                                        //println("rango 3")
+                                    }
+                                }
+                                pressPieDer.update()
+                            }
+                            Log.d("PRUEBA", "DocumentSnapshot data: ${document.data}")
+                        } else {
+                            Log.d("PRUEBA", "No such document")
+                        }
+                    }
+/*
+                //Obtener el array de temperatura Izquierda
+                val pressi = db2.collection("ns").document("${nsi}").collection("${FirebaseAuth.getInstance().uid}").document("press")
+                pressi.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null){
+                            val list_press = document["pizq"] as ArrayList<Number>?
+                            Log.d("PRUEBAMESTA", list_press.toString())
+
+                            if (list_press != null) {
+                                //Establecer rango de color segun el valor leido
+                                var a = 0
+                                for (p in list_press) {
+                                    a = a + 1
+                                    if(p.toInt() <= 15){
+                                        println(p)
+                                        println("rango 1")
+                                    }
+                                    if(p.toInt() in 16..20){
+                                        var opi1: PathModel = pressPieIzq.getPathModelByName("opi" + a)
+                                        opi1.setFillColor(Color.parseColor("#fcba03"))
+                                        println(p)
+                                        println("rango 2")
+                                    }
+                                    if(p.toInt() > 20){
+                                        var opi2: PathModel = pressPieIzq.getPathModelByName("opi" + a)
+                                        opi2.setFillColor(Color.parseColor("#ED4337"))
+                                        println(p)
+                                        println("rango 3")
+                                    }
+                                }
+                                //Refrescar los colores de los vectores
+                                pressPieIzq.update()
+                                pressPieDer.update()
+                            }
+                            Log.d("PRUEBA", "DocumentSnapshot data: ${document.data}")
+                        } else {
+                            Log.d("PRUEBA", "No such document")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.d("PRUEBA", "get failed with ", exception)
+                    }
+                */
+            } else {
+                Log.d("TEST", "No such document")
+            }
+        }
+        return view
+
     }
 
     companion object {
